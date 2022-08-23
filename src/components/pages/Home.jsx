acb5356c8f92450be47445e938fc0593aaa11a7a
noraceppic <messanora86@gmail.com>
2022-08-23 18:34:17 +0200

    ajout page Home

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,24 @@
+import './Home.css'
+export default function Home() {
+  return (
+    <>
+      <h1>La science fondamentale, occupation non contingente</h1>
+      <div id="paragraphe">
+      <p>
+        La livraison des premières images du télescope spatial James-Webb est
+        l’occasion de rappeler que ce type de recherche n’a rien de superflu.
+        Elle est l’honneur de l’humanité qui, dans ces défis, peut trouver
+        l’occasion de dépasser ses rivalités voire ses divisions.
+      </p>
+      <p>
+        Enfin ! Plus de trente ans après que des astronomes ont imaginé le mettre
+        un jour en orbite, le télescope spatial James-Webb (JWST), fruit d’une
+        collaboration entre les agences spatiales américaine, européenne et
+        canadienne, a livré ses premières images, lundi 11 et mardi 12 juillet.
+        Et quelles images : nébuleuses, amas de galaxies, lentilles
+        gravitationnelles… comme on ne les avait jamais vues !
+      </p>
+      </div>
+    </>
+  );
+}
