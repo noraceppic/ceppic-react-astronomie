1c2a9b5f2426137fe7cfcf8eebf9768154a1b3fa
noraceppic <messanora86@gmail.com>
2022-08-23 20:13:24 +0200

    formulaire articles

--- src/components/pages/Ajouter.jsx
+++ src/components/pages/Ajouter.jsx
@@ -1,6 +1,16 @@
 export default function Ajouter() {
-    return(
-        <h1> Ajouter Articles</h1>
-    )
-    
-};
+  return (
+    <>
+      <h1> Ajouter Articles</h1>
+
+      <div className="AjouterArticle">
+      <label for="Titre"> Titre:</label>
+      <input type="text" name="Titre" required />
+      </div>
+      <div className="article">
+      <label for="Article"> Article:</label>
+      <textarea  name="Article" required></textarea > 
+      </div>
+    </>
+  );
+}
