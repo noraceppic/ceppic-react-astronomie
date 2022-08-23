f0da7efec964e92d79c40245bd5712e1c88b3928
noraceppic <messanora86@gmail.com>
2022-08-23 11:51:05 +0200

    ajout style nav barre

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,30 @@
+
 import {Link} from "react-router-dom";
+import"./Header.css"
 
 export default function Header() {
     return(
         <>
         <nav id="NAV">
+            <ul id="headmenu">
+                <li>
             <Link to ="Articles" >
                 <span> Article</span>
             </Link>
+              </li>
+              <li>
             <Link to ="Ajouter" >
                 <span> Ajouter Article</span>
             </Link>
+            </li>
+            
+             <li>
+
              <Link to ="Apropos" >
                 <span> Apropos</span>
             </Link>
-            
+            </li>
+            </ul>
 
 
         </nav>
